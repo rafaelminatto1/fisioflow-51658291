dc418ab29f1a3d2d0d3ca6a00e5a09eac635b602
rafaelminatto1 <rafael@sateg.com.br>
2025-09-10 00:04:07 -0300

    Update build script and sync pnpm-lock.yaml

--- vercel-build.sh
+++ vercel-build.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 # Force pnpm usage on Vercel
-if [ ! -f "node_modules/.bin/vite" ]; then
-  npm install -g pnpm
-  pnpm install
-fi
-p
+npm install -g pnpm
+pnpm install --no-frozen-lockfile
+pnpm run build
