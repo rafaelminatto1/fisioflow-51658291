90e24ea00dfcacf2e19559df30957f8085ae70d7
Rafael Minatto <rafael.minatto@yahoo.com.br>
2026-01-30 17:16:51 -0300

    fix(ios): add metro.config.js for monorepo module resolution

--- apps/professional-ios/metro.config.js
+++ apps/professional-ios/metro.config.js
@@ -3,6 +3,7 @@ const path = require('path');
 
 // Find the project and workspace root
 const projectRoot = __dirname;
+// This can be replaced with `find-up` however for simplicity we just go up one level
 const workspaceRoot = path.resolve(projectRoot, '../..');
 
 const config = getDefaultConfig(projectRoot);
@@ -12,11 +13,12 @@ config.watchFolders = [workspaceRoot];
 
 // 2. Let Metro know where to resolve packages and in what order
 config.resolver.nodeModulesPaths = [
-  path.resolve(projectRoot, 'node_modules'),
-  path.resolve(workspaceRoot, 'node_modules'),
+    path.resolve(projectRoot, 'node_modules'),
+    path.resolve(workspaceRoot, 'node_modules'),
 ];
 
 // 3. Force Metro to resolve (and bundle) packages from their source if needed
+// This is important for packages linked via pnpm/workspace
 config.resolver.disableHierarchicalLookup = true;
 
 module.exports = config;
