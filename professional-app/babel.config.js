da857698f816d847a5a227a7e38f2f5e0c683c28
Rafael Minatto <rafael@rafael-IdeaPad-Slim-3-15ARP10.(none)>
2026-03-17 18:43:11 -0300

    chore(mobile): activate React Compiler and migrate to oxlint for Expo apps

--- professional-app/babel.config.js
+++ professional-app/babel.config.js
@@ -4,6 +4,7 @@ module.exports = function (api) {
   return {
     presets: ['babel-preset-expo'],
     plugins: [
+      'babel-plugin-react-compiler',
       'react-native-reanimated/plugin',
     ],
   };
