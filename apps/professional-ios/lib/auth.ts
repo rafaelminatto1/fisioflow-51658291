fad9c92964a92ae24d574a759ce88f9364328df4
Rafael Minatto <rafael@rafael-IdeaPad-Slim-3-15ARP10.(none)>
2026-03-17 19:38:33 -0300

    chore: consolidate e2e tests and remove redundant apps folder

--- apps/professional-ios/lib/auth.ts
+++
@@ -1,5 +0,0 @@
-import { authClient } from './neonAuth';
-
-export async function signOut(_auth?: unknown) {
-  await authClient.signOut();
-}
--- apps/professional-ios/lib/functions.ts
+++
@@ -1,5 +0,0 @@
-export function httpsCallable(_functions: unknown, name: string) {
-  return async (_payload: unknown) => {
-    throw new Error(`Callable '${name}' ainda não possui endpoint Cloudflare equivalente no app profissional.`);
-  };
-}
