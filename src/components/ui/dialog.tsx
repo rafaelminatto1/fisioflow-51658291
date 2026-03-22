ce9216c5ce0412a3bf602a46fe6c41b633311896
Rafael Minatto <rafael@rafael-IdeaPad-Slim-3-15ARP10.(none)>
2026-03-22 16:33:54 -0300

    Extract dialog utilities stylesheet

--- src/components/ui/dialog.tsx
+++ src/components/ui/dialog.tsx
@@ -2,6 +2,7 @@ import * as React from "react";
 import * as DialogPrimitive from "@radix-ui/react-dialog";
 import { X } from "lucide-react";
 import { cn } from "@/lib/utils";
+import "@/styles/dialog-utilities.css";
 
 const Dialog = DialogPrimitive.Root;
 
