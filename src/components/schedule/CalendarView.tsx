167a0c2499feec91940dd4279ae2fc26036c30eb
Rafael Minatto <rafael@rafael-IdeaPad-Slim-3-15ARP10.(none)>
2026-03-25 05:16:56 -0300

    fix: restore standard exports and lazy imports for calendar components

--- src/components/schedule/CalendarView.tsx
+++ src/components/schedule/CalendarView.tsx
@@ -1290,5 +1290,5 @@ function calendarViewAreEqual(
 	);
 }
 
-export const CalendarView = memo(CalendarViewBase, calendarViewAreEqual);
-CalendarView.displayName = "CalendarView";
+export default memo(CalendarViewBase, calendarViewAreEqual);
+CalendarViewBase.displayName = "CalendarView";
