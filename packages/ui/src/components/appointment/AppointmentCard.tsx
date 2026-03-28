6e03ae0f70cf1a13e2d0f839683ea62c15beb55e
Rafael Minatto <rafael@rafael-IdeaPad-Slim-3-15ARP10.(none)>
2026-03-28 00:49:19 -0300

    fix(web): define appointment card style prop

--- packages/ui/src/components/appointment/AppointmentCard.tsx
+++ packages/ui/src/components/appointment/AppointmentCard.tsx
@@ -35,6 +35,7 @@ export const AppointmentCard = React.forwardRef<HTMLDivElement, AppointmentCardP
     onClick,
     statusConfig,
     compact = false,
+    style,
     className,
     children,
     ...props
