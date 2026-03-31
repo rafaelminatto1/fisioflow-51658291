b5235109a39b3cd05e048c8baeefb374b636a385
Rafael Minatto <rafael@rafael-IdeaPad-Slim-3-15ARP10.(none)>
2026-03-31 02:25:31 -0300

    fix: explicit type conversion in Swift module to resolve compilation error 65

--- src/components/analysis/dicom/initCornerstone.ts
+++ src/components/analysis/dicom/initCornerstone.ts
@@ -4,20 +4,20 @@ let initialized = false;
 
 type CornerstoneInitRuntime = {
 	core: typeof import("@cornerstonejs/core");
-	tools: typeof import("@cornerstonejs/tools");
+	toolsInit: () => void;
 	dicomImageLoader: typeof import("@cornerstonejs/dicom-image-loader");
 };
 
 export default async function initCornerstone({
 	core,
-	tools,
+	toolsInit,
 	dicomImageLoader,
 }: CornerstoneInitRuntime) {
 	if (initialized) return;
 
 	try {
 		await core.init();
-		await tools.init();
+		await toolsInit();
 		dicomImageLoader.init({
 			maxWebWorkers:
 				typeof navigator !== "undefined" && navigator.hardwareConcurrency
